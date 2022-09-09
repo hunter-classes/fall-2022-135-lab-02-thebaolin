666be050da6e8fc4c417bae61f9e33c4277fb85b
thebaolin <bchang916@gmail.com>
2022-09-08 22:08:25 -0400

    fibonacci.cpp

---
+++ fibonacci.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+  int fib[60];
+  fib[0] = 0;
+  fib[1] = 1;
+
+  for(int i=2; i<60; i++){
+    fib[i] = fib[i-1] + fib[i-2];
+  }
+  for(int i=0; i<60; i++){
+    std::cout<<fib[i] << " ";
+  }
+  std::cout<<"\n";
+}
